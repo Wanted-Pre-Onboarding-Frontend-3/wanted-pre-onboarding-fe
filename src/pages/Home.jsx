88028ae1af9276bcba7492461f18c0bbb4f58415
owen <cockyblind@gmail.com>
2022-08-13 11:43:38 +0900

    ✨useInputs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,37 @@
 import { Anchor, Button, Input } from "components";
-import React from "react";
+import React, { useState } from "react";
+import { api } from "services";
+import { hooks } from "utils";
 
 export default function Home() {
+  const { inputs, handleInput } = hooks.useInputs();
+
+  const handleForm = async (e) => {
+    e.preventDefault();
+    const res = await api.signIn(inputs);
+  };
+
   return (
     <main className="w-screen h-screen flex flex-col justify-center items-center">
-      <form className="w-96 shadow-2xl rounded-2xl flex flex-col items-center gap-4 py-6 px-6">
+      <form
+        className="w-96 shadow-2xl rounded-2xl flex flex-col items-center gap-4 py-6 px-6"
+        onSubmit={handleForm}
+      >
         <h1 className="text-3xl">로그인</h1>
-        <Input className="focus:outline-sky-400" placeholder="이메일" />
-        <Input className="focus:outline-sky-400" placeholder="비밀번호" />
+        <Input
+          className="focus:outline-sky-400"
+          placeholder="이메일"
+          name="email"
+          value={inputs.email}
+          onChange={handleInput}
+        />
+        <Input
+          className="focus:outline-sky-400"
+          placeholder="비밀번호"
+          name="password"
+          value={inputs.password}
+          onChange={handleInput}
+        />
         <div className="flex gap-4 w-full">
           <Button className="bg-sky-400 text-white" type="submit">
             로그인
--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,6 +1,17 @@
 import { AuthContext } from "components/AuthProvider";
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 export function useAuth() {
   return React.useContext(AuthContext);
+}
+
+export function useInputs(initialValue = {}) {
+  const [inputs, setInputs] = useState(initialValue);
+
+  const handleInput = useCallback((e) => {
+    const { name, value } = e.target;
+    setInputs((prev) => ({ ...prev, [name]: value }));
+  }, []);
+
+  return { inputs, handleInput };
 }
